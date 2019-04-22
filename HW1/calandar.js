d47ff07ca60610d2d642839fd6bcd73254f04b15
sglee487 <sglee487@gmail.com>
2019-04-22 16:59:04 +0900

    notebook_window merge

--- HW1/calandar.js
+++ HW1/calandar.js
@@ -1,3 +1,18 @@
+﻿var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd='0'+dd
+}
+
+if(mm<10) {
+    mm='0'+mm
+}
+
+today = mm+'/'+dd+'/'+yyyy;
+
 var table_caption;
 table_caption = document.getElementById("table_caption");
-table_caption.innerHTML="2019년 4월";
+table_caption.innerHTML=today;
