9c815f399be0e9306ea9de18483d0a932eb8d1e6
sg <sglee487@gmail.com>
2019-04-26 12:14:17 +0900

    날짜 안 스케쥴 넣는방법 구상 (#9)

--- HW1/calandar.js
+++ HW1/calandar.js
@@ -31,35 +31,13 @@ table_caption.innerHTML=today;
    // 첫번째 cell_1 을 0부터 시작 안하고 1부터 시작해서 생기는 부작용인듯.. 그래도 그냥 가자.
    var today_cell = start_cell + dd - 1;
    var now = new Date();
-var firstDate, lastDate;
 
-firstDate = new Date(now.getYear(), now.getMonth(),1).getDate();
-lastDate = new Date(now.getYear(), now.getMonth()+1,0).getDate();
+   var end_day = cal_end_day();
+   var days = new Array();
+   for (var i=0;i < end_day;i++) {
+     days[i] = document.getElementById("cell_" + (start_cell+i));
+     days[i].innerHTML=i+1;
 
-//alert(firstDate + "/" + lastDate);
-    // alert(start_cell-2);
-  // alert(typeof(start_cell));
-
- // var cell_today = document.getElementById("cell_" + (today_cell));
- // // today = document.getElementById("cell_" + 5 + "");
- // var cell_yesterday = document.getElementById("cell_" + (today_cell-1));
- // // yesterday = document.getElementById("cell_" + (today_cell-1));
- // var cell_yesterday_and_before = document.getElementById("cell_" + (today_cell-2));
- // var cell_tommrow = document.getElementById("cell_" + (today_cell+1));
- // var cell_tommorow_and_after = document.getElementById("cell_" + (today_cell+2));
- //
- // cell_today.innerHTML="todayyyyyyyyy" + day + "  " + dd;
- // cell_yesterday.innerHTML="yesterdayddd ";
- // cell_yesterday_and_before.innerHTML="and before";
- // cell_tommrow.innerHTML="tommrow";
- // cell_tommorow_and_after.innerHTML="and after";
- //test.innerHTML=d;
-
-var end_day = cal_end_day();
- var days = new Array();
- for (var i=0;i < end_day;i++) {
-   days[i] = document.getElementById("cell_" + (start_cell+i));
-   days[i].innerHTML=i+1;
    if ((i+1) < dd) {
      days[i].style.backgroundColor = "#e3e4ea";
    } else if ((i+1) == dd) {
@@ -73,4 +51,56 @@ var end_day = cal_end_day();
 
 function cal_end_day() {
   return new Date(now.getYear(), now.getMonth()+1,0).getDate();
+}
+
+
+var html_body = document.getElementsByTagName('body');
+
+function make_backgroundcolor_gray() {
+  html_body[0].style.backgroundColor = '#b0b0b0';
+  for (var i=0;i < end_day;i++) {
+    if ((i+1) < dd) {
+      days[i].style.backgroundColor = "#9e9ea3";
+    } else if ((i+1) == dd) {
+      days[i].style.backgroundColor = "#689eb0";
+    } else {
+      days[i].style.backgroundColor = "#97a18f";
+    }
+  }
+}
+make_backgroundcolor_gray();
+
+function make_backgroundcolor_original() {
+  html_body[0].style.backgroundColor = '#ffffff';
+  for (var i=0;i < end_day;i++) {
+    if ((i+1) < dd) {
+      days[i].style.backgroundColor = "#e3e4ea";
+    } else if ((i+1) == dd) {
+      days[i].style.backgroundColor = "#96e3ff";
+    } else {
+      days[i].style.backgroundColor = "#d9e8ce";
+    }
+
+  }
+}
+
+make_backgroundcolor_original();
+
+var testp = document.getElementById("day_add");
+testp.style.backgroundColor = "white";
+testp.style.display = "none";
+
+
+var testdiv = document.getElementById("test_div");
+testdiv.style.color = "red";
+
+testdiv.insertAdjacentHTML('beforeend','<div id="tow">tow</div>');
+
+// 각 날마다 4 크기 배열을 가지는 빈 박스 object 만들고,
+// for문을 돌려 안에 내용이 없으면 none, 있으면 표시 하게 하는게 나을것 같다.
+// 왜냐하면 나중에 자동 순서 배치도 있어야 하고, 순서도 바꿀수 있어야 하기 때문.
+// 근데 지금 너무 졸리다 아 자고실다 아ㅣ지너라ㅏㅣ런ㅇㄹㅇ너ㅏㅣㅣ 질꺼야 말리지마 
+function schedulebox(var day) {
+  this.date = new Date(now.getYear(), now.getMonth(), day);
+  this.priority;
 }
