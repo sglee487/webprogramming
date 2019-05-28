67ab29f2a3e2a67a4e2186c734933d86925dfea0
sglee487 <sglee487@gmail.com>
2019-05-28 23:11:55 +0900

    silsup0524 first commit

---
+++ silsup0524/dataFind.php
@@ -0,0 +1,22 @@
+<?php
+
+    echo "GET : ";
+    print_r($_GET);
+
+    echo "<br>"
+
+    echo "REQUEST : ";
+    print_r($_REQUEST);
+
+    echo "<br>"
+
+    echo "POST : ";
+    print_r($_POST);
+
+    echo "<br>"
+    echo $_GET[name] . "<br";
+    echo $_GET[word] . "<br>";
+
+    echo "name : $name <br>";
+    echo "sord : $word <br>";
+    ?>
---
+++ silsup0524/php_Q.js
@@ -0,0 +1,12 @@
+function dataSave() {
+    form.action = "dataSave.php";
+    form.submit();
+}
+function dataFind() {
+    form.action = "dataFind.php";
+    form.submit();
+}
+function allData() {
+    form.action = "allData.php";
+    form.submit();
+}
