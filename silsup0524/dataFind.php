345cd7718a4390c56f87ea787e59767732a5a221
sglee487 <sglee487@gmail.com>
2019-05-29 18:16:11 +0900

    모양은 대충 갖춘것 같은데 에러뜸

--- silsup0524/dataFind.php
+++ silsup0524/dataFind.php
@@ -1,6 +1,9 @@
 <?php
     $myfile = fopen("./data.txt","r");
 
+    $given_name = $_POST["name"];
+    $given_word = $_POST["word"];
+
     $infoArray = array();
     // echo fgets($myfile);  
     while(!feof($myfile)) {
@@ -9,9 +12,29 @@
         // echo $infoArray['aaa'];
     }
     foreach($infoArray as $key => $key_value) {
-        echo $key . "<br>" . $key_value;
-        echo "<br>";
+        findInform($key,$key_value);
     }
+
+
     fclose($myfile);
+
+    function findInform(Str $arrayName, Str $arrayWord) {
+        if ($arrayName == NULL && $arrayWord == NULL) {
+            echo "Enter the keywords of lis that you want to search.";
+        } else if($arrayName == NULL) {
+            if (!((strpos($arrayWord,$given_word) == -1))) {
+                echo $arrayName . "<br>" . $arrayWord;
+            }
+        } else if($_word == NULL) {
+            if (!(strpos($arrayName,$given_name) == -1)) {
+                echo $arrayName . "<br>" . $arrayWord;
+            }
+        } else {
+            if (!(strpos($arrayName,$given_name) == -1) || !(strpos($arrayWord,$given_word) == -1)) {
+                echo $arrayName . "<br>" . $arrayWord;
+            }
+        }
+    }
+
 
 ?>
