7af821e6eeb15f17b17026d9fbf089b729a3dc86
sglee487 <sglee487@gmail.com>
2019-05-29 14:13:04 +0900

    파일 저장까지 성공

--- silsup0524/dataSave.php
+++ silsup0524/dataSave.php
@@ -6,26 +6,28 @@
 // echo $word
 // echo $name;
 // echo $word;
-echo "jklfkjlfkjdfsklfdskl";
+// echo "jklfkjlfkjdfsklfdskl";
 
 $myfile = fopen("./data.txt","a");
 
 class FileInfo {
     // $myfile = fopen("./data.txt","a");
     function FileInfo() {
+        $this->myfile = fopen("./data.txt","a");
         $this->name = $_POST['name'];
         $this->word = $_POST['word'];
     }
-    // function saveInfo(Str $_name,Str $_word) {
-    //     fwrite($myfile,$_name);
-    //     fwrite($myfile,$_word);
-    //     fclose($myfile);
-    //     echo "저장되었습니다.";
-    // }
+    function saveInfo() {
+        fwrite($myfile,$this->name);
+        fwrite($myfile,$this->word);
+        fclose($myfile);
+        echo $this->name;
+        echo "저장되었습니다.";
+    }
 }
 
-// $fileinfo = new FileInfo();
-// $fileinfo->saveInfo($_POST['name'],$_POST['word']);
+$fileinfo = new FileInfo();
+$fileinfo->saveInfo();
 
 
 ?>
