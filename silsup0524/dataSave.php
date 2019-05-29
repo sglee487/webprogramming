5d086c27fc9ff6018485b6f610e6d793276c7fdf
sglee487 <sglee487@gmail.com>
2019-05-29 14:06:35 +0900

    클래스에 변수넣기 성공

--- silsup0524/dataSave.php
+++ silsup0524/dataSave.php
@@ -1,6 +1,31 @@
 <?php
 // echo $_POST['name'];
 // echo $_POST['word'];
-echo "저장되었습니다."
+
+// echo $name
+// echo $word
+// echo $name;
+// echo $word;
+echo "jklfkjlfkjdfsklfdskl";
+
+$myfile = fopen("./data.txt","a");
+
+class FileInfo {
+    // $myfile = fopen("./data.txt","a");
+    function FileInfo() {
+        $this->name = $_POST['name'];
+        $this->word = $_POST['word'];
+    }
+    // function saveInfo(Str $_name,Str $_word) {
+    //     fwrite($myfile,$_name);
+    //     fwrite($myfile,$_word);
+    //     fclose($myfile);
+    //     echo "저장되었습니다.";
+    // }
+}
+
+// $fileinfo = new FileInfo();
+// $fileinfo->saveInfo($_POST['name'],$_POST['word']);
+
 
 ?>
