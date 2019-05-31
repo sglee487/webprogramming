252dad22af511d3e2ec942a6369a95f85f9635b1
sglee487 <sglee487@gmail.com>
2019-05-31 10:28:16 +0900

    아이디나 패스워드 미 입력시 경고문 출력

--- HW2/SignUp.php
+++ HW2/SignUp.php
@@ -2,6 +2,14 @@
 
 $inputUserId = $_POST["UserId"];
 $inputUserPassword = $_POST["UserPassword"];
-echo $inputUserId . " " . $inputUserPassword . "<br>";
+// echo $inputUserId . " " . $inputUserPassword . "<br>";
+if ($inputUserId == "" || $inputUserPassword == "") {
+    echo "아이디 또는 패스워드를 입력해 주세요.";
+} else {
+    $myfile = fopen("./data/person.txt","a");
+
+}
+
+
 
 ?>
