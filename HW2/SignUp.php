036aad3059f836effa106578250895a57c8e1733
sglee487 <sglee487@gmail.com>
2019-05-31 11:12:19 +0900

    회원 저장 성공

--- HW2/SignUp.php
+++ HW2/SignUp.php
@@ -6,8 +6,36 @@ $inputUserPassword = $_POST["UserPassword"];
 if ($inputUserId == "" || $inputUserPassword == "") {
     echo "아이디 또는 패스워드를 입력해 주세요.";
 } else {
-    $myfile = fopen("./data/person.txt","a");
+    $myfile_r = fopen("./data/person.txt","r");
 
+    $noDuplId = TRUE;
+    while(!feof($myfile_r)) {
+        $tempReadId = trim(fgets($myfile_r));
+        echo "읽은 \$tempReadId 값은 : " . $tempReadId . "타입은 : " . gettype($tempReadId) . "<br>";
+        if($tempReadId == NULL) {
+            echo "if(\$tempReadId == NULL) 안으로 들어왔다. <br>";
+            break;
+        }
+        echo "읽은 \$inputUserId 값은 : " . $inputUserId . "타입은 : " . gettype($inputUserId) . "<br>";
+        if ($inputUserId == $tempReadId) {
+            echo "if(\$inputUserId == \$tempReadId) 안으로 들어왔다. <br>";
+            $noDuplId = FALSE;
+            break;
+        }
+        // fgets($myfile_r);
+        echo "읽은 password 값: " . fgets($myfile_r) . "<br>";
+    }
+    fclose($myfile_r);
+
+    if (!$noDuplId) {
+        echo "이미 존재하는 아이디가 있습니다.";
+    } else {
+        $myfile_a = fopen("./data/person.txt","a");
+        fwrite($myfile_a, $inputUserId . "\n");
+        fwrite($myfile_a, $inputUserPassword . "\n");
+        fclose($myfile_a);
+        echo "성공적으로 저장되었습니다.";
+    }
 }
 
 
