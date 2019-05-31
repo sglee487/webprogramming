14c296ab772d668d27a601918a830ecdca0fb0da
sglee487 <sglee487@gmail.com>
2019-05-31 10:02:49 +0900

    디버그용 출력문 및 필요없는 주석들 지움

--- silsup0524/dataFind.php
+++ silsup0524/dataFind.php
@@ -5,35 +5,21 @@
     $given_word = $_POST["word"];
 
     $infoArray = array();
-    // echo fgets($myfile);  
     while(!feof($myfile)) {
-        // echo fgets($myfile) . fgets($myfile);
         $temp_str = fgets($myfile);
         if ($temp_str != NULL) {
             $infoArray[$temp_str] = fgets($myfile);
         }
-        // $infoArray[fgets($myfile)] = fgets($myfile);
-        // echo $infoArray['aaa'];
     }
 
     if ($given_name == "" && $given_word == "") {
         echo "Enter the keywords of lis that you want to search.";
     } else{
         foreach($infoArray as $key => $key_value) {
-            echo "key: " . $key . " value: " . $key_value . "<br>";
+            // echo "key: " . $key . " value: " . $key_value . "<br>";
             findInform($key,$key_value);
         }
     } 
-    // else if($given_word == "") {
-    //     if (!(strpos($given_name,$given_name) == -1)) {
-    //         echo $argument[0] . "<br>" . $argument[1];
-    //     }
-    // } else {
-    //     if (!(strpos($argument[0],$given_name) == -1) || !(strpos($argument[1],$given_word) == -1)) {
-    //         echo $argument[0] . "<br>" . $argument[1];
-    //     }
-    // }
-
 
     fclose($myfile);
     
@@ -41,8 +27,6 @@
     function findInform($argument1, $argument2) {
         $given_name = $_POST["name"];
         $given_word = $_POST["word"];
-        // echo "given_name: " . $given_name . " given_word: " . $given_word . "<br>";
-        // echo "argument1 : " . $argument1 . " argument2: " . $argument2 . "<br>";
 
         // 여기에서 입력값이 있을때랑 없을때로 나눠보자.
         if ($given_name == "") {
@@ -62,20 +46,6 @@
                 echo "<br>" . $argument1 . "<br>" . $argument2 . "<br>";
             }
         }
-
-        // if($given_name == "") {
-        //     if ((strpos($argument[1],$given_word) == -1)) {
-        //         echo $argument[0] . "<br>" . $argument[1];
-        //     }
-        // } else if($given_word == "") {
-        //     if (!(strpos($given_name,$given_name) == -1)) {
-        //         echo $argument[0] . "<br>" . $argument[1];
-        //     }
-        // } else {
-        //     if (!(strpos($argument[0],$given_name) == -1) || !(strpos($argument[1],$given_word) == -1)) {
-        //         echo $argument[0] . "<br>" . $argument[1];
-        //     }
-        // }
     }
 
 
