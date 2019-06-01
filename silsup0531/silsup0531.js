2c887b60218372ad4e0759b5318f8a832bf98387
sglee487 <sglee487@gmail.com>
2019-06-01 10:58:17 +0900

    box1로 담는데는 성공

--- silsup0531/silsup0531.js
+++ silsup0531/silsup0531.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
     var pic_upload = $("#pic_upload");
+    
+    
 });
 
 function showImage() {
@@ -14,8 +16,25 @@ function showImage() {
                 // alert(file.name);
                 var AddImageTag = document.createElement("img");
                 AddImageTag.setAttribute('src','./' + file.name);
+                AddImageTag.setAttribute('id','img' + i);
+                AddImageTag.setAttribute('draggable','true');
+                AddImageTag.setAttribute('ondragstart','drag(event)');
                 pic_upload.appendChild(AddImageTag);
             }
         }
     }
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("img",ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("img");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
 }
