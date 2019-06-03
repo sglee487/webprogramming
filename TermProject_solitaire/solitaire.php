1152b1381a5c7754753d4a5fcd8226bab330f2b0
sglee487 <sglee487@gmail.com>
2019-06-03 23:46:07 +0900

    파일 쓰고 읽으려면 php 써야 할듯..

---
+++ TermProject_solitaire/solitaire.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="kr">
+    <head>
+        <title>웹 텀. 솔리테어.</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="solitaire.css">
+    </head>
+    <body>
+        <div class="maintainBoard">
+            <table id="table1">
+                    <!-- 카드보드 위에 부분. -->
+                <thead> 
+                    <tr>
+                        <td class="card_normal" id="back_card"></td>
+                        <td class="card_normal" id="front_card"></td>
+                        <td class="card_empty"></td>
+                        <td class="card_results" id="result1"></td>
+                        <td class="card_results" id="result2"></td>
+                        <td class="card_results" id="result3"></td>
+                        <td class="card_results" id="result4"></td>
+                    </tr>
+                </thead>
+                <tbody >
+                    <tr class="card_normal">
+                        <td id="process1">a</td>
+                        <td id="process2">b</td>
+                        <td id="process3">c</td>
+                        <td id="process4">d</td>
+                        <td id="process5">e</td>
+                        <td id="process6">f</td>
+                        <td id="process7">g</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+
+    </body>
+</html>
