cf94858c5511f571477b5051dddcab7b3acbb710
sglee487 <sglee487@gmail.com>
2019-06-11 03:04:46 +0900

    셔플 함수 만드는중...

--- TermProject_solitaire/solitaire.js
+++ TermProject_solitaire/solitaire.js
@@ -9,3 +9,17 @@ $(document).ready(function(){
     process2.text("hellop2");
 });
 
+var allCards = []
+// 총 52개의 카드가 있으므로, 52개를 담을 수 있는 배열을 만든다.
+function cardsInit() {
+    for (var i=1; i <= 52; i++) {
+        allCards.push(i);
+        // 이제 allCards엔 [1,2,3,4,...] 이 들어있다. 1부터 해야 카드 순서 안헷갈림.
+    }
+}
+
+// 위의 allCards가 랜덤 배열로 되어야 섞어서 넣을때도 랜덤으로 됨.
+// 서로 자리를 바꾸는 형식으로 하면 중복될 일이 없다.
+function suffleCards(array) {
+
+}
