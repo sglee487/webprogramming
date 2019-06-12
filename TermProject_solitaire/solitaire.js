f0628b11d452cb432f475f7320c13c0875a3ce0e
sglee487 <sglee487@gmail.com>
2019-06-12 16:20:28 +0900

    suffleCards 함수 구현

--- TermProject_solitaire/solitaire.js
+++ TermProject_solitaire/solitaire.js
@@ -21,5 +21,17 @@ function cardsInit() {
 // 위의 allCards가 랜덤 배열로 되어야 섞어서 넣을때도 랜덤으로 됨.
 // 서로 자리를 바꾸는 형식으로 하면 중복될 일이 없다.
 function suffleCards(array) {
+    var temp, randomNumber;
+    for (var i=0;i < array.length; i++) {
+        randomNumber = Math.floor(Math.random() * (array.length+1));
+        temp = array[randomNumber];
+        array[randomNumber] = array[i];
+        array[i] = temp;
+    }
+    return array;
+}
+
+// 처음 게임을 시작했을 때 카드를 계단 형식으로 놓도록 함..
+function putCardsInit(){
 
 }
