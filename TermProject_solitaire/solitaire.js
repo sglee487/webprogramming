9a862af86e64d97c3d1b66ad542883aa0bdc712a
sglee487 <sglee487@gmail.com>
2019-06-15 22:13:15 +0900

    카드들 뒤집는거 까지 완료

--- TermProject_solitaire/solitaire.js
+++ TermProject_solitaire/solitaire.js
@@ -23,6 +23,7 @@ $(document).ready(function(){
 var allCards = [];
 var allCards_id = [];
 var flipBackCards_id = [];
+var flipFrontCards_id = [];
 
 var resultCardCount = 0;
 
@@ -111,23 +112,37 @@ function putCardsInit(){
     // console.log("allCards_id.length : " + allCards_id.length + ", flipBackCards_id.length : " + flipBackCards_id.length);
     // console.log("allCards_id.length + flipBackCards_id.length : " + allCards_id.length + flipBackCards_id.length);
     // 처음 back_card id에 붙이는 카드.
-    document.getElementById("back_card").appendChild($("<div class='card' id=" + flipBackCards_id[0] + " value = 3></div>")[0]);
-    for (var index = 1; index < flipBackCards_id.length; index++) {
-        document.getElementById(flipBackCards_id[(index-1)]).appendChild($("<div class='card' id=" + flipBackCards_id[index] + " value = 3></div>")[0]);
+    // document.getElementById("back_card").appendChild($("<div class='card' id=" + flipBackCards_id[0] + " value = 3></div>")[0]);
+    for (var index = 0; index < flipBackCards_id.length; index++) {
+        // document.getElementById(flipBackCards_id[(index-1)]).appendChild($("<div class='card' id=" + flipBackCards_id[index] + " value = 3></div>")[0]);
+        $("<div class='card' id=" + flipBackCards_id[index] + " value = 3></div>").appendTo("#back_card");
         $("#" + flipBackCards_id[index]).css("background-image","url(data/0_0.jpg)");
-        $("#" + flipBackCards_id[index]).attr("draggable","false");
-        $("#" + flipBackCards_id[index]).attr("ondragstart","");
-        $("#" + flipBackCards_id[index]).attr("ondrop","");
-        $("#" + flipBackCards_id[index]).attr("ondragover","");
+        
+        // $("#" + flipBackCards_id[index]).attr("draggable","false");
+        // $("#" + flipBackCards_id[index]).attr("ondragstart","");
+        // $("#" + flipBackCards_id[index]).attr("ondrop","");
+        // $("#" + flipBackCards_id[index]).attr("ondragover","");
+        
+        $("#" + flipBackCards_id[index]).css("position","absolute");
+        $("#" + flipBackCards_id[index]).css("top","23px");
 
-        $("#" + flipBackCards_id[index]).attr("onclick","flipCard(" + flipBackCards_id[index] + ")");
+        $("#" + flipBackCards_id[index]).attr("onclick","flipCard('" + flipBackCards_id[index] + "')");
         // $("#" + flipBackCards_id[index]).attr("ondragstart","drag(event)");
         // $("#" + flipBackCards_id[index]).attr("ondrop","drop(event)");
         // $("#" + flipBackCards_id[index]).attr("ondragover","allowDrop(event)");
         // $("<div class='card' id=" + flipBackCards_id[index] + " value = 3></div>").appendTo("#back_card");
         // $("#" + flipBackCards_id[index]).css("background-image","url(data/0_0.jpg)");
     }
+    // $("#" + flipBackCards_id[23]).attr("onclick","flipCard('" + flipBackCards_id[23] + "')");
+}
 
+function flipCard(cardId_str) {
+    console.log("cardId_str : " + cardId_str);
+    $("#" + cardId_str).appendTo("#front_card");
+    // $("#front_card")[0].appendChild($("#" + cardId_str)[0]);
+    $("#" + cardId_str).css("top","23px");
+    $("#" + cardId_str).css("background-image","url(data/" + cardId_str + ".jpg)");
+    
 }
 
 function allowDrop(ev) {
@@ -330,9 +345,18 @@ function revealLastCardInProcessId(oldClickedCardParent) {
 }
 
 function setCardBackgroundProperty() {
+
     setResultBackground();
     setProcessBackground();
 
+}
+
+function setBackCard() {
+    $("#back_card").attr("onclick","refillCards()");
+}
+
+function refillCards() {
+    console.log("refill cards");
 }
 
 function setResultBackground() {
